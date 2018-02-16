34b3b3510ff8bc7b9e31c4267a0f8017ea41a953
emre <test@example.com>
2018-02-15 22:05:00 -0600

    create generateMessage uility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -12,14 +13,23 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-   console.log('new user');
+   console.log('new user connected');
+
+   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+   
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+  });
 
    socket.on('disconnect', () => {
       console.log('User was disconnected');
    });
 });
-
-
 
 server.listen(port, () => {
    console.log(`Server is up on port ${port}`);
